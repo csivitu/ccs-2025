44a104a8b309d73c887f92df96cd06b2993ec1ac
har-sat <harsat.ponnusamy@gmail.com>
2025-01-21 23:08:10 +0530

    chore: added actual links to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,16 +1,36 @@
 import SocialMediaLink from "./socialMediaLink";
+import githubLogo from "@/app/domains/assets/gitHubLogo.svg";
+import linkedInLogo from "@/app/domains/assets/linkedinLogo.svg";
+import instagramLogo from "@/app/domains/assets/instagramLogo.svg";
+import xLogo from "@/app/domains/assets/xLogo.svg";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-row min-w-full bg-slate-800 py-10 mt-auto">
-      <SocialMediaLink />
-      <SocialMediaLink />
-      <div className="flex flex-col flex-grow text-center">
+    <footer className="flex flex-row min-w-full bg-slate-800 py-6 mt-auto">
+      <SocialMediaLink
+        name="@csivitu"
+        href="https://www.instagram.com/csivitu/"
+        icon={instagramLogo}
+      />
+      <SocialMediaLink
+        name="@csivitu"
+        href="https://x.com/csivitu"
+        icon={xLogo}
+      />
+      <div className="flex flex-col flex-grow text-center justify-center">
         <p className="text-2xl">When we build, it matters.</p>
         <p className="text-slate-500">© 2025. Made with 🤍 @CSI</p>
       </div>
-      <SocialMediaLink />
-      <SocialMediaLink />
+      <SocialMediaLink
+        name="@csivitu"
+        href="https://github.com/csivitu"
+        icon={githubLogo}
+      />
+      <SocialMediaLink
+        name="Computer Society of India"
+        href="https://www.linkedin.com/company/csivitu/posts/"
+        icon={linkedInLogo}
+      />
     </footer>
   );
 }
