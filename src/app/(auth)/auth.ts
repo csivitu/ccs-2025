b08c9ff0f291ae5f8219faf5786b7b24880a9497
TejasGhatte <tejasghatte5@gmail.com>
2025-02-06 21:42:45 +0530

    feat: next auth revamp

---
+++ src/app/(auth)/auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/db";
+
+export const {handlers, auth, signIn, signOut} = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    trustHost: true,
+    providers: [
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: 'https://accounts.google.com/o/oauth2/auth?response_type=code&hd=vitstudent.ac.in'
+        }),
+    ],
+});
