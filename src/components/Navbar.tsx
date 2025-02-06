0b81d08121836314e145878f097230963817a42d
har-sat <harsat.ponnusamy@gmail.com>
2025-02-06 20:33:06 +0530

    chore: reduce navbar scale

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,8 @@ import { IoBookOutline, IoCubeOutline } from "react-icons/io5";
 import { LuTable2 } from "react-icons/lu";
 import { FaRegStar } from "react-icons/fa";
 
+import Image from "next/image";
+
 const Navbar = () => {
   const [activeLink, setActiveLink] = useState("");
 
@@ -13,8 +15,14 @@ const Navbar = () => {
       <div className="mx-auto flex flex-col">
         <div className="flex justify-between items-center">
           <div className="flex items-center space-x-2">
-            <img src="/git.webp" alt="Logo" className="h-12 w-auto" />
-            <span className="text-3xl">csivitu</span>
+            <Image
+              src="/git.webp"
+              width={25}
+              height={25}
+              alt="Logo"
+              className="h-8 w-auto"
+            />
+            <span className="text-2xl text-center px-2">csivitu</span>
           </div>
           <div className="flex items-center space-x-4">
             <a href="#home" className="font-bold text-xl">
@@ -23,7 +31,7 @@ const Navbar = () => {
             <a href="#faqs" className="font-bold text-xl">
               FAQs
             </a>
-            <img src="/giticon.webp" alt="FAQs Icon" className="h-12 w-auto" />
+            <Image src="/giticon.webp" width={50} height={50} alt="FAQs Icon" className="h-8 w-auto" />
           </div>
         </div>
 
@@ -37,7 +45,7 @@ const Navbar = () => {
             <div key={item.id} className="relative">
               <a
                 href={`#${item.id}`}
-                className={`text-lg flex items-center space-x-2 ${
+                className={`text-md flex items-center space-x-2 ${
                   activeLink === item.id
                     ? "text-[#C9D1D9]"
                     : "text-[#C9D1D9] hover:text-white transition-colors"
@@ -45,7 +53,7 @@ const Navbar = () => {
                 onClick={() => setActiveLink(item.id)}
               >
                 <item.Icon
-                  size={28}
+                  size={22}
                   color={
                     activeLink === item.id
                       ? "rgba(139, 148, 158, 1)"
@@ -67,8 +75,6 @@ const Navbar = () => {
             </div>
           ))}
         </div>
-        {/* <div className="mt-2 h-0.5 w-full bg-gray-800" /> */}
-        {/* <div className="mt-1 h-0.5 w-full bg-gray-800" /> */}
       </div>
     </nav>
   );
