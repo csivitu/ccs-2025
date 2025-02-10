87ada4f90db1619aa3b88389b98e21106d561fad
har-sat <harsat.ponnusamy@gmail.com>
2025-02-11 00:03:20 +0530

    refactor: add navlink component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,20 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
-import FaRegStar from "public/logos/navbar-logos/FaRegStar.svg"
-import IoBookOutline from "public/logos/navbar-logos/IoBookOutline.svg"
-import LuTable from "public/logos/navbar-logos/LuTable.svg"
-import IoCubeOutline from "public/logos/navbar-logos/IoCubeOutline.svg"
 
+import FaRegStar from "public/logos/navbar-logos/FaRegStar.svg";
+import IoBookOutline from "public/logos/navbar-logos/IoBookOutline.svg";
+import LuTable from "public/logos/navbar-logos/LuTable.svg";
+import IoCubeOutline from "public/logos/navbar-logos/IoCubeOutline.svg";
+
+import NavLink from "./nav-link";
+
+const navLinks = [
+  { id: "about", label: "About Us", Icon: IoBookOutline },
+  { id: "projects", label: "Projects", Icon: LuTable },
+  { id: "domains", label: "Domains", Icon: IoCubeOutline },
+  { id: "alumni", label: "Alumni", Icon: FaRegStar },
+];
 
 const Navbar = () => {
   const [activeLink, setActiveLink] = useState("");
@@ -32,50 +41,26 @@ const Navbar = () => {
             <a href="#faqs" className="font-semibold text-xl">
               FAQs
             </a>
-            <Image src="/giticon.webp" width={50} height={50} alt="FAQs Icon" className="h-8 w-auto" />
+            <Image
+              src="/giticon.webp"
+              width={50}
+              height={50}
+              alt="FAQs Icon"
+              className="h-8 w-auto"
+            />
           </div>
         </div>
 
         <div className="flex space-x-12 mt-6">
-          {[
-            { id: "about", label: "About Us", Icon: IoBookOutline },
-            { id: "projects", label: "Projects", Icon: LuTable },
-            { id: "domains", label: "Domains", Icon: IoCubeOutline },
-            { id: "alumni", label: "Alumni", Icon: FaRegStar },
-          ].map((item) => (
-            <div key={item.id} className="relative">
-              <a
-                href={`#${item.id}`}
-                className={`text-md flex items-center space-x-2 ${
-                  activeLink === item.id
-                    ? "text-[#C9D1D9]"
-                    : "text-[#C9D1D9] hover:text-white transition-colors"
-                }`}
-                onClick={() => setActiveLink(item.id)}
-              >
-                <Image
-                  src={item.Icon}
-                  alt={item.id}
-                  style={{ width: "1.25em", height: "1.25em" }}
-                  color={
-                    activeLink === item.id
-                      ? "rgba(139, 148, 158, 1)"
-                      : "rgba(139, 148, 158, 1)"
-                  }
-                  className="mr-1" // Added margin-right to the icon
-                />
-                <span className="relative">
-                  {item.label}
-                  <div
-                    className={`absolute -bottom-1 left-0 w-[110%] h-0.5 transform transition-all duration-300 ${
-                      activeLink === item.id
-                        ? "bg-[rgba(247,129,102,1)] scale-x-110"
-                        : "bg-transparent scale-x-100"
-                    }`}
-                  />
-                </span>
-              </a>
-            </div>
+          {navLinks.map((item) => (
+            <NavLink
+              key={item.id}
+              id={item.id}
+              label={item.label}
+              Icon={item.Icon}
+              isActive={activeLink === item.id}
+              onLinkClick={() => setActiveLink(item.id)}
+            />
           ))}
         </div>
       </div>
--- src/types/domain-card-props.ts
+++ src/types/domain-card-props.ts
@@ -1,3 +1,5 @@
+import { StaticImageData } from "next/image";
+
 export interface domainCardProps {
   domainName: string;
   domainIcon: StaticImageData;
