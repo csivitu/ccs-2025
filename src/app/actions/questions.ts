d7659dfb9234d633b0b00798768a4a28633d7dd1
TejasGhatte <tejasghatte5@gmail.com>
2025-02-12 11:09:03 +0530

    feat: fixed actions

---
+++ src/app/actions/questions.ts
@@ -0,0 +1,92 @@
+"use server"
+
+import { DomainType, PrismaClient } from '@prisma/client';
+import {
+  domainSchema,
+  questionIdSchema,
+  userIdSchema,
+  answerSchema,
+  submitSchema
+} from '@/lib/questions';
+import { auth } from '../(auth)/auth';
+import { revalidatePath } from 'next/cache';
+import ActionResponse from '@/lib/action-response';
+import { prisma } from '@/lib/db';
+
+export async function getQuestionsByDomain(domain: DomainType) {
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      revalidatePath('/auth')
+    }
+    const parsed = questionIdSchema.safeParse(domain);
+    if (!parsed.success) {
+      return ActionResponse.error(400, parsed.error.message, "Invalid domain");
+    }
+
+    const questions = await prisma.question.findMany({ where: { domain } });
+
+    return ActionResponse.success(questions, 200);
+  } catch (err) {
+    const error = err as Error;
+    return ActionResponse.error(500, error?.message, "Error fetching questions");
+  }
+}
+
+export async function getQuestionById(id: string) {
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      revalidatePath('/auth')
+    }
+    const parsed = questionIdSchema.safeParse(id);
+    if (!parsed.success)
+      return { status: 400, error: parsed.error.format() };
+  
+    const question = await prisma.question.findUnique({ where: { id } })
+  
+    if (!question) {
+      return ActionResponse.error(404, "Question not found");
+    }
+  
+    return ActionResponse.success(question, 200);
+  } catch (err) {
+    const error = err as Error;
+    return ActionResponse.error(500, error?.message, "Error fetching question");
+  }
+}
+
+export async function submitQuestion(data:{questionId: string, answer: string}) {
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      revalidatePath('/auth')
+    }
+    const userId = session?.user.id || '';
+    const parsed = submitSchema.safeParse(data);
+    if (!parsed.success)
+      return ActionResponse.error(400, parsed.error.message, "Invalid data");
+
+    const question = await prisma.question.findUnique({ where: { id: data.questionId } });
+
+    if (!question) {
+      return ActionResponse.error(404, "Question not found");
+    }
+
+    const questionAttempt = await prisma.attempedQuestion.upsert({
+      where: {
+        userId_questionId: {
+          questionId: data.questionId,
+          userId: userId,
+        },
+      },
+      update: { answer: data.answer },
+      create: { questionId: data.questionId, userId: userId, answer: data.answer },
+    })
+
+    return ActionResponse.success(questionAttempt, 200);
+  } catch (err) {
+    const error = err as Error;
+    return ActionResponse.error(500, error?.message, "Error submitting question");
+  }
+}
---
+++ src/app/actions/tasks.ts
@@ -0,0 +1,92 @@
+"use server"
+
+import { SubDomain } from '@prisma/client';
+import { submitTaskSchema,
+  subDomainSchema,
+  getTaskIdSchema
+ } from '@/lib/tasks';
+import { prisma } from '@/lib/db'
+import ActionResponse from '@/lib/action-response';
+import { auth } from '../(auth)/auth';
+import { revalidatePath } from 'next/cache';
+
+export async function submitTask(data: { userId: string; task: string[]; taskId: string }) {
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      revalidatePath('/auth')
+    }
+    const parsed=submitTaskSchema.safeParse(data);
+    if(!parsed.success)
+    {
+      return ActionResponse.error(400, parsed.error.message);
+    }
+  
+    const taskSubmission = await prisma.attemptedTask.upsert({
+      where: {
+        userId_taskId: {
+          userId: data.userId,
+          taskId: data.taskId,
+        },
+      },
+      update: {
+        taskSubmission: data.task
+      },
+      create: {userId:data.userId, taskId: data.taskId, taskSubmission:data.task },
+    })
+    
+    return ActionResponse.success(taskSubmission, 201);
+  } catch (err) {
+    const error = err as Error;
+    return ActionResponse.error(500, error?.message, "Error submitting task");
+  }
+}
+
+export async function getTaskById(id: string) {
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      revalidatePath('/auth')
+    }
+    const parsed = getTaskIdSchema.safeParse(id);
+    if (!parsed.success) {
+      return ActionResponse.error(400, parsed.error.message);
+    }
+    const task = await prisma.task.findUnique({ where: { id } });
+    if (!task) {
+      return ActionResponse.error(404, "Task not found");
+    }
+    return ActionResponse.success(task, 200);
+  } catch (err) {
+    const error = err as Error;
+    return ActionResponse.error(500, error?.message, "Error fetching task");
+  }
+}
+
+export async function getTasksBySubDomain(subDomain: SubDomain) {
+  try {
+    const session = await auth();
+    if (!session?.user) {
+      revalidatePath('/auth')
+    }
+    const parsed=subDomainSchema.safeParse(subDomain);
+    if (!parsed.success ) {
+        return { success:false, error: parsed.error.format() };
+      }
+    const tasks = await prisma.task.findMany({
+      where: { subDomain },
+    });
+
+    if (!tasks || tasks.length === 0) {
+      return ActionResponse.error(404, "Tasks not found");
+    }
+
+    return ActionResponse.success(tasks, 200);
+  } catch (err) {
+    const error = err as Error;
+    return ActionResponse.error(500, error?.message, "Error fetching tasks");
+    
+  }
+}
+
+
