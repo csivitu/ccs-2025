18de0b21b77ebdcc72cd5669a2a20b0f4cfdb08c
Nidhi2805 <nidhianand.karva2023@vitstudent.ac.in>
2025-02-13 12:41:52 +0530

    import path fix

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,8 @@
 import SocialMediaLink from "./socialMediaLink";
-import githubLogo from "public/logos/gitHubLogo.svg";
-import linkedInLogo from "public/logos/linkedinLogo.svg";
-import instagramLogo from "public/logos/instagramLogo.svg";
-import xLogo from "public/logos/xLogo.svg";
+import githubLogo from "../../../public/logos/githubLogo.svg";
+import linkedInLogo from "../../../public/logos/linkedInLogo.svg";
+import instagramLogo from "../../../public/logos/instagramLogo.svg";
+import xLogo from "../../../public/logos/xLogo.svg";
 
 export default function Footer() {
   return (
