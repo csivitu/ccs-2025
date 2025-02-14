5aaac2e76fc82b534e3be4e2d74f6212a8dfb2c2
Nidhi2805 <nidhianand.karva2023@vitstudent.ac.in>
2025-02-14 22:00:05 +0530

    Desktop sizing faq

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Navbar from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
+import Footer from "@/components/footer/footer";
 import faq from "../data/faq";
 
 const FAQ = () => {
@@ -15,10 +15,10 @@ const FAQ = () => {
     <div className="min-h-screen bg-zinc-950 text-white">
       <Navbar />
       <div className="flex justify-center items-center flex-col px-4 py-12">
-        <h1 className="text-6xl font-bold my-4 text-center bg-clip-text text-transparent bg-gradient-to-r from-[#A2FACF] to-[#64ACFF]">
+        <h1 className="text-6xl font-bold my-4 text-center bg-clip-text text-transparent bg-gradient-to-r from-[#A2FACF] to-[#64ACFF] font-sans code">
           CSI's Frequently Asked <br /> Questions
         </h1>
-        <button className="bg-white text-black py-2 px-6 rounded-xl text-xl font-bold mb-8">
+        <button className="bg-white text-black py-2 px-6 rounded-xl text-xl font-bold mb-8 font-sans code">
           Contact Us
         </button>
         <div className="w-full max-w-4xl">
@@ -34,7 +34,7 @@ const FAQ = () => {
                   </span>
                 </span>
                 <span className="flex items-center">
-                  <span className="text-2xl ml-4">
+                  <span className="text-[20px] ml-4 font-sans code">
                     {item.question}
                   </span>
                 </span>
@@ -42,13 +42,15 @@ const FAQ = () => {
               <div
                 className={`overflow-hidden transition-all duration-500 ease-in-out ${openIndex === index ? "max-h-screen" : "max-h-0"}`}
               >
-                <p className="px-6 pb-4 text-lg text-[#76C3F1] ml-12">{item.answer}</p>
+                <p className="px-6 pb-4 text-lg text-[#76C3F1] ml-12 font-sans code">{item.answer}</p>
               </div>
             </div>
           ))}
         </div>
       </div>
+      <div className="mt-[-60px]">
       <Footer />
+      </div>
     </div>
   );
 };
