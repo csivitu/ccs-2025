eaa0b624925fcd753eebe8bdc8716e71b7f3c9f7
manaslaud <manaslaud@hotmail.com>
2025-02-18 14:05:12 +0530

    feat:question and answer panel ui

--- src/components/questions-page/answer-panel.tsx
+++ src/components/questions-page/answer-panel.tsx
@@ -2,11 +2,11 @@ import TextEditor from "./text-editor";
 
 export default function AnswerPanel({ currentIndex, answers, setAnswers }: { currentIndex: number; answers: string[]; setAnswers: React.Dispatch<React.SetStateAction<string[]>> }) {
   return (
-    <div className="w-full h-full bg-[#252526]">
-      <div className="bg-[#1E1E1E] px-6 py-2 m-1 w-fit">
+    <div className="w-full h-full">
+      <div className=" px-6 py-2 m-1 w-fit">
       <p className="flex items-center gap-2">
-          <span className="text-[#519ABA]">TS</span> 
-          <span className="mx-1">Untitled -1</span> 
+          <span className="text-[#519ABA]">Ts</span> 
+          <span className="mx-1">Answer File</span> 
           <span className="text-[#CCCCCC]">X</span>
         </p>
 
