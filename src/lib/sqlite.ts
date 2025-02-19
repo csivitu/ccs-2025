d71592a827b8bdcf3a5feedf700bdaf258d41371
TejasGhatte <tejasghatte5@gmail.com>
2025-02-19 23:00:19 +0530

    feat: sqlite3 db init

---
+++ src/lib/sqlite.ts
@@ -0,0 +1,27 @@
+import sqlite3 from "sqlite3";
+import { open, Database } from "sqlite";
+import path from "path";
+
+let db: Database;
+
+export async function connectDB() {
+  if (!db) {
+    const dbPath = path.join(process.cwd(), "src", "data", "csi-enrolled-students.db");
+    console.log(dbPath)
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+  }
+  return db;
+}
+
+export async function findUserByEmail(email: string) {
+  const db = await connectDB();
+  return db.get("SELECT * FROM users WHERE email = ?", [email]);
+}
+
+export async function getUserPhoneNumber(email: string) {
+  const db = await connectDB();
+  return db.get("SELECT mobile_no FROM users WHERE email = ?", [email]);
+}
