772d9fe3e1076db65c1aec200d2d4717fbf61783
TejasGhatte <tejasghatte5@gmail.com>
2025-02-19 23:00:35 +0530

    feat: callbacks pre and post signin

--- src/app/(auth)/auth.ts
+++ src/app/(auth)/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/db";
+import { findUserByEmail, getUserPhoneNumber } from "@/lib/sqlite";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
@@ -13,4 +14,33 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
             authorization: 'https://accounts.google.com/o/oauth2/auth?response_type=code&hd=vitstudent.ac.in'
         }),
     ],
+    callbacks: {
+        async signIn({ user, account }) {
+            if (account?.provider === "google") {
+                const email = user.email;
+                if (!email){
+                    return false
+                }
+                const userExists = await findUserByEmail(email);
+                if (!userExists) {
+                    return false;
+                }
+            }
+            return true;
+        },
+
+        async session({ session, user }) {
+            if (session.user) {
+                const phoneNo = await getUserPhoneNumber(session.user.email);
+                if(!phoneNo) return session
+                await prisma.user.update({
+                    where: { id: user.id },
+                    data: {
+                        phoneNumber: String(phoneNo),
+                    }
+                });
+            }
+            return session;
+        }
+    }
 });
