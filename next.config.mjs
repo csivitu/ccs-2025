0edbb43f3d3042b5b842e5e22d6702828f900411
Keshav-Aneja <keshav.aneja09@gmail.com>
2025-02-19 23:18:10 +0530

    feat: profile image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["i.ibb.co","imgbb.com","ibb.co"]
+        domains: ["i.ibb.co","imgbb.com","ibb.co","lh3.googleusercontent.com"]
     }
 };
 
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,14 +1,20 @@
 import { getUserStats } from "../../actions/domains";
 import { redirect } from "next/navigation";
 import ProfileClient from "./profile";
+import { auth } from "@/app/(auth)/auth";
+import { User } from "lucide-react";
 
 const Profile = async () => {
   const response = await getUserStats();
   if (!response || !response.success || !response.data) {
     redirect("/");
   }
-
-  return <ProfileClient user={response.data} />;
+    const session = await auth();
+    if (!session?.user) {
+      redirect("/");
+    }
+  
+  return <ProfileClient user={response.data} image={session.user.image || ""} />;
 };
 
 export default Profile;
