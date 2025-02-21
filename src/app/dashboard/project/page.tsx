c95611ff914a381dda49dd35f4cbd28726e45b31
woaitsAryan <yoprince.ab@gmail.com>
2025-02-21 17:38:25 +0000

    feat: updated project details

--- src/app/dashboard/project/page.tsx
+++ src/app/dashboard/project/page.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { FaRegStar } from "react-icons/fa";
 import { IoMdGitNetwork } from "react-icons/io";
-import { projects } from "@/data/projects";
+import { projects, Languages } from "@/data/projects";
 import Link from "next/link";
 
 const ProjectPage = () => {
+   function getLanguageColor(language: string) {
+     switch (language) {
+       case Languages.JAVASCRIPT:
+         return "rgba(218, 91, 11, 1)";
+       case Languages.TYPESCRIPT:
+         return "rgba(0, 122, 209, 1)";
+       case Languages.PYTHON:
+         return "rgba(255, 193, 7, 1)";
+       case Languages.GO:
+         return "rgba(0, 122, 209, 1)";
+       default:
+         return "rgba(255, 193, 7, 1)";
+     }
+   }
   return (
-    <div className="h-fit min-h-screen w-full bg-[#0A0A0A]">
+    <div className="min-h-screen w-full bg-[#0A0A0A]">
       <div className="flex flex-col gap-8 md:gap-0 md:flex-row px-4 lg:px-10 mt-6 md:mt-12">
-        <div className="ml-2 lg:w-1/4 text-white">
+        <div className="ml-2 lg:w-1/4 text-white lg:sticky lg:top-0 md:h-screen">
           <h1 className="text-white font-sans-code text-2xl lg:text-[36px] font-semibold leading-[30px] mb-2 lg:mb-4">
             Projects
           </h1>
@@ -58,7 +72,7 @@ const ProjectPage = () => {
           </div>
         </div>
 
-        <div className="lg:w-[70%]  lg:mt-[-8px] min-h-screen lg:min-h-[calc(100vh-10rem)]">
+        <div className="lg:w-[70%]  lg:mt-[-8px] h-screen lg:h-[calc(100vh-10rem)]">
           <div className="border-[3px] border-[#30363D] rounded-[6px] ">
             <div className="h-full">
               {projects.map((project, index) => (
@@ -92,24 +106,9 @@ const ProjectPage = () => {
                         <div className="flex items-center">
                           <span
                             className="w-2 h-2 mr-1 rounded-full"
-                            style={{ backgroundColor: "rgba(218, 91, 11, 1)" }}
+                            style={{ backgroundColor: getLanguageColor(project.language) }}
                           />
                           <span className="text-[12px] text-[#8B949E] font-sans-code">
-                            Tech Domain
-                          </span>
-                        </div>
-                        <div className="flex items-center">
-                          <FaRegStar size={16} className="text-gray-500" />
-                          <span className="ml-1 text-[12px] text-[#8B949E] font-sans-code">
-                            Popular
-                          </span>
-                        </div>
-                        <div className="flex items-center">
-                          <IoMdGitNetwork
-                            size={16}
-                            className="text-[#8B949E]"
-                          />
-                          <span className="ml-1 text-[12px] text-[#8B949E] font-sans-code">
                             {project.language}
                           </span>
                         </div>
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,41 +1,43 @@
+export enum Languages {
+  JAVASCRIPT = "Javascript",
+  TYPESCRIPT = "Typescript",
+  PYTHON = "Python",
+  GO = "Go"
+}
+
 export const projects = [
     {
       "projectTitle": "Capture the Flag challenges",
       "projectDescription": "An aggregation of the Capture The Flag challenges and write-ups for CSICTF 2020",
-      "techDomain": "Cybersec",
-      "language": "Javascript",
+      "language": Languages.JAVASCRIPT,
       "builtBy": "Rohan Mukherjee",
       "repoLink": "https://github.com/csivitu/ctf-challenges"
     },
     {
       "projectTitle": "Gravitas Lasertag",
       "projectDescription": "The Backend for this year's Gravitas Lasertag",
-      "techDomain": "Webdev (Backend)",
-      "language": "Javascript (Express)",
+      "language": Languages.JAVASCRIPT,
       "builtBy": "Yug Oswal",
       "repoLink": "https://github.com/csivitu/lasertag-gravitas-csi"
     },
     {
       "projectTitle": "Kustom",
       "projectDescription": "Customizable Form Builder - Forms on steroids",
-      "techDomain": "Webdev",
-      "language": "Next.js",
+      "language": Languages.TYPESCRIPT,
       "builtBy": "Keshav Aneja",
       "repoLink": "https://kustom.cc"
     },
     {
       "projectTitle": "Code Tester",
       "projectDescription": "A CLI designed to test user code against user defined inputs",
-      "techDomain": "CLI-Tools",
-      "language": "Typescript, Bashscript",
+      "language": Languages.TYPESCRIPT,
       "builtBy": "Ashikka Gupta, Rohan Mukherjee, Rahil Kabani",
       "repoLink": "https://github.com/csivitu/code-executor"
     },
     {
       "projectTitle": "Classic Arcade Games",
       "projectDescription": "A number of classic Games written in a myriad of languages",
-      "techDomain": "Gamedev",
-      "language": "Javascript, Python, Java, C#, C",
+      "language": Languages.PYTHON,
       "builtBy": "Everyone",
       "repoLink": "https://github.com/csivitu/ArcadeGames"
     },
@@ -43,16 +45,14 @@ export const projects = [
     {
       "projectTitle": "Core Committee Selection",
       "projectDescription": "The Website you are looking at",
-      "techDomain": "Webdev (Frontend)",
-      "language": "Next.js, Javascript, CSS",
+      "language": Languages.TYPESCRIPT,
       "builtBy": "Nidhi Karva, Tejas Ghatte, Ishita Babar, Harsh Shah, Neeladri Mullick, Aditi Saxena, Shreeyash Ugale, Harsat Ponnusamy, Adhavan",
       "repoLink": "https://github.com/csivitu/CCS-Frontend-2022"
     },
     {
       "projectTitle": "Evie",
       "projectDescription": "A Calendar website made for VIT Club events.",
-      "techDomain": "Webdev",
-      "language": "Javascript (React.js, Express)",
+      "language": Languages.JAVASCRIPT,
       "builtBy": "Sanjay Kumar Baskaran, Mannan Goyal",
       "repoLink": "https://github.com/csivitu/Evie-Frontend"
     }
